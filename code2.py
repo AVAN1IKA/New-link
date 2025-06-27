524f1fa827037c733cdbfcda63e21a2f7e127bba
Avan1ika <avantikameena006@gmail.com>
2025-06-27 15:09:44 +0530

    Add python file code2.py to New-link

---
+++ code2.py
@@ -0,0 +1,8 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Multiply the numbers
+product = num1 * num2
+
+print("The product is:", product)
