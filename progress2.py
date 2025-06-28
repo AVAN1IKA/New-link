5c801a8583721ab201a13a7c7dd0079bb5b9f3b3
AVAN1IKA <167649510+AVAN1IKA@users.noreply.github.com>
2025-06-28 20:33:03 +0530

    Update progress2.py

--- progress2.py
+++ progress2.py
@@ -12,3 +12,7 @@ while True:
 #OUTPUT: if you not stop it , this will continue showing you output one after another 
 #TERMINAL: if you not stop the terminal the output will continue running like a digital watch 
 #the differnece is we see new output with past output in OUTPUT DISPLAY whereas in TERMINAL we see the time actually changing like a digital watch , where the seconds are changing 
+
+#THE_PROBLEM_FACED
+#the python extension was not running as i didnt have python on my pc
+#after troubleshooting i dowloaded it and get the issue sorted
