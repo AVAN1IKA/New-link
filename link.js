371beb9d211877aaf3da373957f5bf9fc39a97a7
AVAN1IKA <167649510+AVAN1IKA@users.noreply.github.com>
2025-06-28 20:35:35 +0530

    Update link.js

--- link.js
+++ link.js
@@ -3,3 +3,14 @@ let num2 = 10;
 let sum = num1 + num2;
 
 console.log(sum);
+
+//GIT_ACTION
+//this is a second PYHTON file 
+//so i used following COMMANDS 
+//git status (this will show untracked file that the changes are stagged but not commited yet)
+//git add code.py
+//git commit "Add python file code2.py to New-link"
+//git push origin main
+
+//for pulling changes in vs code 
+//git pull origin main 
