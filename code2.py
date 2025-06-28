847beacab76b09ffb7d3fbb44fefabf6f64d6f5c
Avan1ika <avantikameena006@gmail.com>
2025-06-28 18:15:09 +0530

    Local changes before pull

--- code2.py
+++ code2.py
@@ -7,17 +7,9 @@ product = num1 * num2
 
 print("The product is:", product)
 
-#AS THE OUTPUT NEEDS INPUT VALUES 
-#WE USED TERMINAL FOR BY RIGHT CLICK FOR RUNNING OUTPUT 
 
-#THE_PROBLEM_FACED
-#the python extension was not running as i didnt have python on my pc
-#after troubleshooting i dowloaded it and get the issue sorted
 
-#GIT_ACTION
-#this is a second PYHTON file 
-#so i used following COMMANDS 
-#git status (this will show untracked file that the changes are stagged but not commited yet)
-#git add code.py
-#git commit "Add python file code2.py to New-link"
-#git push origin main
+
+
+
+
